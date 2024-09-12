43ca4074322e7ce32d7414459eeb60aed7884563
shidqitio <shidqitio@gmail.com>
2024-09-12 10:42:56 +0700

    Get Menu Level 2 dan Level 3

--- src/routes/api/refMenu2-route.ts
+++ src/routes/api/refMenu2-route.ts
@@ -7,6 +7,7 @@ updatedRefMenu2Schema,
 destroyRefMenu2Schema,
 searchRefMenu2Schema,
 getRefMenu2Schema,
+paramsLevelSchema
 } from "@schema/api/refMenu2-schema"
 const routes = express.Router()
 
@@ -19,6 +20,8 @@ routes.post("/", validate(payloadRefMenu2Schema), refMenu2Controller.store)
 routes.get("/get-menu1/:id", validate(getRefMenu2Schema), refMenu2Controller.getByKodeMenu1)
 
 routes.put("/:id", validate(updatedRefMenu2Schema), refMenu2Controller.update)
+
+routes.get("/menu-level2/:id1/:id2", validate(paramsLevelSchema), refMenu2Controller.menuByLevel2)
 
 routes.delete("/:id", validate(destroyRefMenu2Schema), refMenu2Controller.destroy)
 
